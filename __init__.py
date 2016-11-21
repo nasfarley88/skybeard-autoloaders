603ff5e13782f74a919fbedf6f011b1c06d86a45
Nathanael Farley <nasfarley88@gmail.com>
2016-11-21 13:11:39 +0000

    Added (untested!) branch support.

--- __init__.py
+++ __init__.py
@@ -11,18 +11,20 @@ class AutoLoader(object):
     pass
 
 class Git(AutoLoader):
-    def __init__(self, url, import_as=None):
+    def __init__(self, url, import_as=None, branch=None):
         "Creates a temporary directory full of a git repo."
 
         self.url = url
         logger.debug("Creating temporary directory")
         self.path = tempfile.mkdtemp()
         logger.info("Importing {} using git".format(self.url))
-        if import_as is None:
-            sp.check_call("/usr/bin/git -C {dir} clone {url}".format(dir=self.path, url=self.url).split(" "))
-        else:
-            sp.check_call("/usr/bin/git -C {dir} clone {url} {import_as}".format(
-                dir=self.path, url=self.url, import_as=import_as).split(" "))
+        git_cmd ="/usr/bin/git -C {dir} clone {url}".format(dir=self.path, url=self.url).split(" ") 
+        if import_as is not None:
+            git_cmd.append(import_as)
+        if branch is not None:
+            git_cmd.extend(["-b", branch])
+
+        sp.check_call(git_cmd)
 
     def __del__(self):
         logger.debug("Deleting temporary directory {}".format(self.path))
